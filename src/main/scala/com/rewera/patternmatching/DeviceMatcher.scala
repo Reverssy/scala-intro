06e436072ff56a5edc6fe20995eb7b77bb57f433
Maciek <ReweraM@gmail.com>
2018-06-23 11:21:20 +0200

    Pattern Matching

---
+++ src/main/scala/com/rewera/patternmatching/DeviceMatcher.scala
@@ -0,0 +1,31 @@
+package com.rewera.patternmatching
+
+object DeviceMatcher {
+  sealed abstract class Device
+  case  class Phone(model: String) extends Device {
+    def screenOff = "Turning screen off"
+  }
+  case class Computer(model: String) extends Device {
+    def screenSaverOn = "Turning screen saver on"
+  }
+
+  def goIdle(device: Device): String = {
+    device match {
+      case p: Phone =>
+        p.screenOff
+      case c: Computer =>
+        c.screenSaverOn
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println("START")
+    val phone = Phone("iPhone 6s")
+    val comp = Computer("MacBook Pro 2015")
+
+    println(goIdle(phone))
+    println(goIdle(comp))
+  }
+
+}
