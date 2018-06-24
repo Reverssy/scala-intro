386bc36b6ef55ee4cb88323f30d5d344c1ea075e
Maciek <ReweraM@gmail.com>
2018-06-24 20:44:24 +0200

    First parallel example

--- src/main/scala/com/rewera/parallaelism/Application.scala
+++ src/main/scala/com/rewera/parallaelism/Application.scala
@@ -1,6 +1,30 @@
 package com.rewera.parallaelism
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Random, Success}
+
 object Application extends App {
-  println("Hi, this is parallelism tester.")
+  println("Computation started...")
+
+  val f = Future {
+    Thread.sleep(Random.nextInt(500))
+    41
+  }
+
+  println("Before onComplete...")
+  f.onComplete {
+    case Success(value) => println(s"Received result: $value")
+    case Failure(e) => e.printStackTrace()
+  }
+
+  println("A..."); Thread.sleep(100)
+  println("B..."); Thread.sleep(100)
+  println("C..."); Thread.sleep(100)
+  println("D..."); Thread.sleep(100)
+  println("E..."); Thread.sleep(100)
+  println("F..."); Thread.sleep(100)
+  Thread.sleep(2000)
+
 
 }
